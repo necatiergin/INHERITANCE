31b28dca80bbe72ffcee60935bc13ff31df5c9cd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-23 18:17:40 +0300

    Create dynamic_cast_02.cpp

---
+++ rtti/dynamic_cast_02.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Base {
+public:
+	virtual ~Base() = default;
+	//
+};
+
+class Foo : public Base {
+public:
+	void f()
+	{
+		std::cout << "Foo::f()\n";
+	}
+	//...
+};
+
+class Bar : public Base {
+public:
+	void b()
+	{
+		std::cout << "Bar::b()\n";
+	}
+	//...
+};
+
+void func(Base* p)
+{
+	if (Foo* fptr = dynamic_cast<Foo*>(p)) {
+		fptr->f();
+	}
+	else if (Bar* bptr = dynamic_cast<Bar*>(p)) {
+
+	}
+}
+
+int main()
+{
+
+	auto foo_ptr = new Foo;
+	auto bar_ptr = new Bar;
+
+	func(foo_ptr);
+	func(bar_ptr);
+
+	delete foo_ptr;
+	delete bar_ptr;
+}
