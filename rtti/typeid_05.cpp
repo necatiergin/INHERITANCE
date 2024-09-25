725a2af88b75fdb57c1b363902c555c05334059f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-25 18:12:01 +0300

    Update typeid_05.cpp

--- rtti/typeid_05.cpp
+++ rtti/typeid_05.cpp
@@ -6,8 +6,8 @@ int main()
 	//type_info has no default ctor
 	//std::type_info x; //invalid
 
-	//copy members are deleetd.
-	//auto x = typeid(20); //invalis
+	//copy members are deleted.
+	//auto x = typeid(20); //invalid
 	//std::type_info& tr = typeid(int); //invalid, const & olmalı
 	const std::type_info& tr1 = typeid(int); //valid
 	auto& tr2 = typeid(int); //valid
