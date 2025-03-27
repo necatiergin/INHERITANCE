1055cffd1cddee20580753bb488b2176695059b3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-27 18:35:56 +0300

    Update us_05_02.cpp

--- using/us_05_02.cpp
+++ using/us_05_02.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+class Base {
+public:
+	Base(int, int)
+	{
+		std::cout << "Base(int, int)\n";
+	}
+
+	Base(double)
+	{
+		std::cout << "Base(double)\n";
+	}
+};
+
+class Der : public Base {
+public:
+	using Base::Base;
+	//...
+};
+
+int main()
+{
+	Der d1(1, 2);
+	Der d2(3.4);
+}
